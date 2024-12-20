83a385d82faa0a5e433bde53126ccbe439381cee
BugHunter-X <askzenai@gmail.com>
2024-12-20 08:42:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+// Use express.json() middleware to parse JSON request bodies
+app.use(express.json());
+app.post('/data', (req, res) => {
+  console.log(req.body); 
+  res.send('Data received');
+});
+app.listen(3000, () => console.log('Server listening on port 3000'));
